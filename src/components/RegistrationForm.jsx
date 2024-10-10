cbc0ed254b1c46d251dd284cb5407c5a55a16015
K2E7 <k2e7@outlook.com>
2024-10-10 23:59:37 +0530

    updating frontend for integration.

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -6,26 +6,31 @@ import "./styles/RegistrationForm.css"
 const RegistrationForm = () => {
 
     const [user, setUser] = useState({
-        firstName: '',
-        lastName: '',
+        username: '',
         email: '',
         password: ''
     });
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
-        setUser({
-            ...user,
+        setUser((prevUser) => ({
+            ...prevUser,
             [name]: value
-        });
+        }));
     };
 
     const handleRegister = async () => {
+        if (!user.username || !user.email || !user.password) {
+            console.log('Please fill out all fields.');
+            return;
+        }
         try {
-            const response = await axios.post('http://localhost:8080/api/users/register', user);
+            const response = await axios.post('http://localhost:8080/api/auth/signup', user);
             console.log('User registered successfully:', response.data);
+            // Optionally, redirect the user or display a success message
         } catch (error) {
-            console.error('Error registering user:', error);
+            console.error('Error registering user:', error.response.data);
+            // Handle the error, e.g., display an error message to the user
         }
     };
 
@@ -33,16 +38,40 @@ const RegistrationForm = () => {
         <div className="r-div-form">
             <div className="r-inputWrapper">
                 <div className="r-form-elem">
-                    <div className="r-formLabel">Username</div>
-                    <input className="r-textfield" type="text" onChange={handleInputChange} id="username" />
+                    <div className="r-formLabel">
+                        Username
+                    </div>
+                    <input className="r-textfield"
+                        type="text"
+                        name="username"
+                        id="username"
+                        value={user.username}
+                        onChange={handleInputChange}
+                    />
                 </div>
                 <div className="r-form-elem">
-                    <div className="r-formLabel">Email</div>
-                    <input className="r-textfield" type="email" onChange={handleInputChange} id="email" />
+                    <div className="r-formLabel">
+                        Email
+                    </div>
+                    <input className="r-textfield" type="email"
+                        name="email"
+                        id="email"
+                        value={user.email}
+                        onChange={handleInputChange}
+                    />
                 </div>
                 <div className="r-form-elem">
-                    <div className="r-formLabel">Password</div>
-                    <input className="r-textfield" type="password" onChange={handleInputChange} id="password" />
+                    <div className="r-formLabel">
+                        Password
+                    </div>
+                    <input
+                        className="r-textfield"
+                        type="password"
+                        name="password"
+                        id="password"
+                        value={user.password}
+                        onChange={handleInputChange}
+                    />
                 </div>
             </div>
 
